5e753008cd30ca28c38c3318557b25adef020c44
a-sha0234 <asharma_31@hotmail.com>
2022-03-17 23:58:43 +0000

    create schema for users

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,2 +1,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+const user = mongoose.model("userdata", userSchema);
+
+module.exports = user;
