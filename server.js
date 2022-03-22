679e7e9220e4cd9c472494cb857e15e2d1de80cc
a-sha0234 <asharma_31@hotmail.com>
2022-03-22 22:27:31 +0000

    remove functions no longer needed

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcryptjs");
+const createPostsRoutes = require("./routes/createPosts");
+const signUpRoutes = require("./routes/sign-up-Routes");
 
 const { check, validationResult, body } = require("express-validator");
 const res = require("express/lib/response");
@@ -30,38 +32,38 @@ mongoose
   .then((result) => app.listen(port))
   .catch((err) => console.log(err));
 
-passport.use(
-  new LocalStrategy((username, password, done) => {
-    User.findOne({ username: username }, (err, user) => {
-      if (err) {
-        return done(err);
-      }
-      if (!user) {
-        return done(null, false, { message: "Incorrect username" });
-      }
-      bcrypt.compareSync(password, user.password, (err, res) => {
-        if (res) {
-          // passwords match! log user in
-          return done(null, user);
-        } else {
-          // passwords do not match!
-          return done(null, false, { message: "Incorrect password" });
-        }
-      });
-      return done(null, user);
-    });
-  })
-);
+// passport.use(
+//   new LocalStrategy((username, password, done) => {
+//     User.findOne({ username: username }, (err, user) => {
+//       if (err) {
+//         return done(err);
+//       }
+//       if (!user) {
+//         return done(null, false, { message: "Incorrect username" });
+//       }
+//       bcrypt.compareSync(password, user.password, (err, res) => {
+//         if (res) {
+//           // passwords match! log user in
+//           return done(null, user);
+//         } else {
+//           // passwords do not match!
+//           return done(null, false, { message: "Incorrect password" });
+//         }
+//       });
+//       return done(null, user);
+//     });
+//   })
+// );
 
-passport.serializeUser(function (user, done) {
-  done(null, user.id);
-});
+// passport.serializeUser(function (user, done) {
+//   done(null, user.id);
+// });
 
-passport.deserializeUser(function (id, done) {
-  User.findById(id, function (err, user) {
-    done(err, user);
-  });
-});
+// passport.deserializeUser(function (id, done) {
+//   User.findById(id, function (err, user) {
+//     done(err, user);
+//   });
+// });
 
 app.use(function (req, res, next) {
   res.locals.currentUser = req.user;
@@ -82,13 +84,13 @@ app.get("/", (req, res) => {
     });
 });
 
-app.get("/sign-up", (req, res) => {
-  res.render("sign-up-form", { title: "sign up", user: req.user, errMsg: "" });
-});
+// app.get("/sign-up", (req, res) => {
+//   res.render("sign-up-form", { title: "sign up", user: req.user, errMsg: "" });
+// });
 
-app.get("/create-posts", (req, res) => {
-  res.render("createPosts", { title: "create posts", user: req.user });
-});
+// app.get("/create-posts", (req, res) => {
+//   res.render("createPosts", { title: "create posts", user: req.user });
+// });
 
 app.post(
   "/log-in",
@@ -100,96 +102,70 @@ app.post(
 
 app.use(express.json());
 
-app.post(
-  "/sign-up",
+// app.post(
+//   "/sign-up",
 
-  body("username").isLength({ min: 3 }).trim().escape(),
-  body("password").isLength({ min: 3 }),
-  body("passwordConfirmation").custom((value, { req }) => {
-    if (req.body.confirmpassword !== req.body.password) {
-      throw new Error("Password confirmation does not match password");
-    }
-    return true;
-  }),
+//   body("username").isLength({ min: 3 }).trim().escape(),
+//   body("password").isLength({ min: 3 }),
 
-  (req, res, next) => {
-    bcrypt.hash("somePassword", 10, (err, hashedPassword) => {
-      // if err, do something
-      // otherwise, store hashedPassword in DB
+//   body("passwordConfirmation").custom((value, { req }) => {
+//     if (req.body.confirmpassword !== req.body.password) {
+//       throw new Error("Password confirmation does not match password");
+//     }
+//     return true;
+//   }),
 
-      // user.find({ username: req.body.username }).then((result) => {
-      //   if (result.username == req.body.username) {
-      //     res.render("sign-up-form", {
-      //       title: "sign up",
-      //       user: req.user,
-      //       errMsg: "user already exists ",
-      //     });
-      //   } else {
-
-      user
-        .find({ username: req.body.username })
-        .then((result) => {
-          if (result[0].username == req.body.username) {
-            res.render("sign-up-form", {
-              title: "sign up",
-              user: req.user,
-              errMsg: "user already exists ",
-            });
-          }
-        })
-        .catch(() => {
-          const errors = validationResult(req);
-          if (!errors.isEmpty()) {
-            return res.status(400).redirect("/sign-up");
-          } else {
-            const user = new User({
-              username: req.body.username,
-              password: hashedPassword,
-              isMember: req.body.isMember,
-            }).save((err) => {
-              if (err) {
-                return next(err);
-              }
-              res.redirect("/");
-            });
-          }
-        });
-    });
-  }
-);
-//       const errors = validationResult(req);
-//       if (!errors.isEmpty()) {
-//         return res.status(400).redirect("/sign-up");
-//       } else {
-//         const user = new User({
-//           username: req.body.username,
-//           password: hashedPassword,
-//           isMember: req.body.isMember,
-//         }).save((err) => {
-//           if (err) {
-//             return next(err);
+//   (req, res, next) => {
+//     bcrypt.hash("somePassword", 10, (err, hashedPassword) => {
+//       user
+//         .find({ username: req.body.username })
+//         .then((result) => {
+//           if (result[0].username == req.body.username) {
+//             res.render("sign-up-form", {
+//               title: "sign up",
+//               user: req.user,
+//               errMsg: "user already exists ",
+//             });
 //           }
-//           res.redirect("/");
+//         })
+//         .catch(() => {
+//           const errors = validationResult(req);
+//           if (!errors.isEmpty()) {
+//             return res.status(400).redirect("/sign-up");
+//           } else {
+//             const user = new User({
+//               username: req.body.username,
+//               password: hashedPassword,
+//               isMember: req.body.isMember,
+//             }).save((err) => {
+//               if (err) {
+//                 return next(err);
+//               }
+//               res.redirect("/");
+//             });
+//           }
 //         });
-//       }
 //     });
 //   }
 // );
 
-app.post("/create-posts", (req, res) => {
-  //send post info to database
-  const user_msg = new userMessage({
-    name: req.user.username,
-    messageSubject: req.body.messageSubject,
-    message: req.body.message,
-  }).save((err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.redirect("/");
-    }
-  });
-});
+// app.post("/create-posts", (req, res) => {
+//   //send post info to database
+//   const user_msg = new userMessage({
+//     name: req.user.username,
+//     messageSubject: req.body.messageSubject,
+//     message: req.body.message,
+//   }).save((err) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       res.redirect("/");
+//     }
+//   });
+// });
+
+app.use(signUpRoutes);
+app.use(createPostsRoutes);
 
 app.get("/log-out", (req, res) => {
   req.logout();
